92ac6622ff3598c32fbf783f387181a8ecc72f18
Doannm <doannm@vnpt-technology.vn>
2023-09-11 10:17:15 +0700

    update proxy

--- AutoLike/Constants/Constants.cs
+++ AutoLike/Constants/Constants.cs
@@ -11,14 +11,14 @@ namespace AutoLike.Constants
         public const string getCurrentProxyShopLike = "https://proxy.shoplike.vn/Api/getCurrentProxy";
         public const string getNewProxyShopLike = "https://proxy.shoplike.vn/Api/getNewProxy";
 
-        public static string GetNewProxyShopLike(string accessToken) 
-        {  
-            return getNewProxyShopLike + "?accessToken=" + accessToken; 
+        public static string GetNewProxyShopLike(string accessToken)
+        {
+            return getNewProxyShopLike + "?accessToken=" + accessToken;
         }
 
         public static string GetCurrentProxyShopLike(string accessToken, string location)
         {
-            return getCurrentProxyShopLike + "?accessToken=" + accessToken + "&&location=" + location ;
+            return getCurrentProxyShopLike + "?accessToken=" + accessToken + "&&location=" + location;
         }
     }
 }
